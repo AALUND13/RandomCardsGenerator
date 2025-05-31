0bdcc905b6b055024e3deff038a639db881edd9d
AALUND13 <tamlund1976@gmail.com>
2025-05-31 13:16:28 -0400

    Patched both `GetCardInfoWithName` and `RPCA_AssignCard`

--- Scripts/Patches/RandomCardSpawningPatches.cs
+++ Scripts/Patches/RandomCardSpawningPatches.cs
@@ -2,15 +2,29 @@
 using ModdingUtils.Patches;
 using Photon.Pun;
 using RandomCardsGenerators.Cards;
+using RandomCardsGenerators.Utils;
 using System;
 using System.Collections.Generic;
+using UnboundLib.Utils;
 using UnityEngine;
 
 namespace RandomCardsGenerators.Patches {
-    [HarmonyPatch(typeof(CardChoice), "Spawn")]
-    public class SpawnPatch {
+    [HarmonyPatch]
+    public class RandomCardSpawningPatches {
+        [HarmonyPatch(typeof(CardChoice), "SpawnUniqueCard")]
+        class SpawnUniqueCardPatch {
+            // Mainly used to detect if the card spawning is in the pick phase
+            public static bool PickPhaseCardSpawning = false;
+
+            [HarmonyPriority(Priority.First)] private static void Prefix() => PickPhaseCardSpawning = true;
+            [HarmonyPriority(Priority.First)] private static void Postfix() => PickPhaseCardSpawning = false;
+
+        }
+
+        [HarmonyPatch(typeof(CardChoice), "Spawn")]
         [HarmonyPriority(Priority.Last)]
-        private static bool Prefix(GameObject objToSpawn, Vector3 pos, Quaternion rot, ref GameObject __result) {
+        [HarmonyPrefix]
+        private static bool Spawn(GameObject objToSpawn, Vector3 pos, Quaternion rot, ref GameObject __result) {
             if(objToSpawn != null && objToSpawn.GetComponent<RandomCard>() != null) {
                 __result = PhotonNetwork.Instantiate(
                     objToSpawn.name,
@@ -23,21 +37,10 @@ namespace RandomCardsGenerators.Patches {
             }
             return true; // Continue with original method
         }
-    }
 
-    [HarmonyPatch(typeof(CardChoice), "SpawnUniqueCard")]
-    public class SpawnUniqueCardPatch {
-        // Mainly used to detect if the card spawning is in the pick phase
-        public static bool PickPhaseCardSpawning = false;
-
-        [HarmonyPriority(Priority.First)] private static void Prefix() => PickPhaseCardSpawning = true;
-        [HarmonyPriority(Priority.First)] private static void Postfix() => PickPhaseCardSpawning = false;
-
-    }
-
-    [HarmonyPatch(typeof(CardChoicePatchGetRanomCard), "OrignialGetRanomCard", new Type[] { typeof(CardInfo[]) })]
-    public class CardChoicePatchGetRanomCardPatch {
-        private static void Prefix(ref CardInfo[] cards) {
+        [HarmonyPatch(typeof(CardChoicePatchGetRanomCard), "OrignialGetRanomCard", new Type[] { typeof(CardInfo[]) })]
+        [HarmonyPrefix]
+        private static void NormalDrawableCardsSpawn(ref CardInfo[] cards) {
             if(!SpawnUniqueCardPatch.PickPhaseCardSpawning) return;
 
             List<CardInfo> list = new List<CardInfo>(cards);
@@ -46,11 +49,10 @@ namespace RandomCardsGenerators.Patches {
             }
             cards = list.ToArray();
         }
-    }
 
-    [HarmonyPatch(typeof(ModdingUtils.Utils.Cards), "AddCardToPlayer", new Type[] { typeof(Player), typeof(CardInfo), typeof(bool), typeof(string), typeof(float), typeof(float), typeof(bool) })]
-    public class AddCardToPlayerPatch {
-        private static bool Prefix(Player player, CardInfo card) {
+        [HarmonyPatch(typeof(ModdingUtils.Utils.Cards), "AddCardToPlayer", new Type[] { typeof(Player), typeof(CardInfo), typeof(bool), typeof(string), typeof(float), typeof(float), typeof(bool) })]
+        [HarmonyPrefix]
+        private static bool AddRandomCardToPlayer(Player player, CardInfo card) {
             if(card.GetComponent<ToggleCustomCard>() != null && (PhotonNetwork.OfflineMode || PhotonNetwork.IsMasterClient)) {
                 foreach(var drawableCard in NormalDrawableRandomCard.NormalDrawableCards) {
                     if(drawableCard.ToggleCard != null && drawableCard.ToggleCard.toggleCardInfo == card) {
@@ -61,6 +63,26 @@ namespace RandomCardsGenerators.Patches {
             }
 
             return true;
+        }
+
+        [HarmonyPatch(typeof(ModdingUtils.Utils.Cards), "RPCA_AssignCard", new Type[] { typeof(string), typeof(int), typeof(bool), typeof(string), typeof(float), typeof(float), typeof(bool) })]
+        [HarmonyPrefix]
+        public static void AssignRandomCardRPC(string cardObjectName, int playerID, bool reassign, string twoLetterCode, float forceDisplay, float forceDisplayDelay, bool addToCardBar) {
+            FindRandomCardsGeneratorResult findResult = RandomCardsUtils.FindRandomCardsGeneratorByName(cardObjectName);
+            if(findResult != null) {
+                Player playerToUpgrade;
+                playerToUpgrade = PlayerManager.instance.players.Find(p => p.playerID == playerID);
+                findResult.RandomCardsGenerator.GenerateRandomCard(findResult.Seed);
+            }
+        }
+
+        [HarmonyPatch(typeof(CardManager), nameof(CardManager.GetCardInfoWithName))]
+        [HarmonyPostfix]
+        public static void GetRandomCardInfoWithName(string cardName, ref CardInfo __result) {
+            FindRandomCardsGeneratorResult findResult = RandomCardsUtils.FindRandomCardsGeneratorByName(cardName);
+            if(findResult != null) {
+                __result = findResult.RandomCardsGenerator.GenerateRandomCard(findResult.Seed).GetComponent<CardInfo>();
+            }
         }
     }
 }
